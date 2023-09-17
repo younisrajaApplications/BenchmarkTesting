1fa22af3619e79807b177f5ffc687c98e1049ebb
younisraja16 <younisraja16@gmail.com>
2023-09-17 22:15:43 +0100

    Benchmark testing Date and time

---
+++ src/benchmark/DateTimeTesting.java
@@ -0,0 +1,22 @@
+package benchmark;
+
+import java.util.Date;
+
+public class DateTimeTesting {
+    public static void main(String[] args) {
+        Date dateOne = new Date();
+        operations();
+        Date dateTwo = new Date();
+
+        System.out.println("Duration: " + (dateTwo.getTime() - dateOne.getTime()) + " ms");
+        System.out.println("Duration: " + ((dateTwo.getTime() - dateOne.getTime())/1000f) + " seconds");
+    }
+    public static void operations(){
+        System.out.println("Started");
+        String x = "";
+        for (int i = 0; i < 1000000; i++) {
+            x += 1;
+        }
+        System.out.println("Completed");
+    }
+}
